8f7d20e0972ac6ab53b8779a7be443bcf4f82e48
AhmedShehap99 <55rrtyy7@gmail.com>
2026-03-29 07:12:34 +0100

    Question02

--- ASSIGNMENT03C#/Program.cs
+++ ASSIGNMENT03C#/Program.cs
@@ -16,6 +16,31 @@
 
 
             #endregion
+            
+                
+            
+
+
+
+            // this code will print integer value x = 9 , lost 0.99. 
+
+
+            #region Question02
+            /*Q2: This code doesn’t compile. Fix it with the smallest change?
+                int n = 5;
+                double d2 = n / 2;
+                Console.WriteLine(d2); */
+
+            //int n = 5;
+            //double d2 = n / 2.0;
+            //Console.WriteLine(d2);
+
+            //// convert value 2 to 2.0 
+
+
+
+            #endregion
+
 
         }
     }
